ba1f7519f1b6a97d1b03954323af1181ca79dc4f
elenacoazzoli <coazzoli.elena@gmail.com>
2021-11-13 09:49:04 +0100

    Styles header for admin link and adds smooth scroll to global style

--- components/Header.tsx
+++ components/Header.tsx
@@ -18,13 +18,14 @@ const LogoContainer = styled.div`
   justify-content: space-between;
 `;
 
-const LogoText = styled.span`
+const LogoText = styled.a`
   font-family: 'Playfair Display', serif;
   color: #343f53;
   font-weight: 900;
   font-size: 3rem;
   padding: 0 0 6px 0;
   box-shadow: inset 0 -12px 0 #efd5d2;
+  cursor: pointer;
 `;
 
 const Logo = styled.img`
@@ -63,6 +64,32 @@ const PageLink = styled.a`
   }
 `;
 
+const AdminContainer = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: #dfe3e9;
+  border-radius: 15px;
+  padding: 6px 16px;
+  margin-top: 12px;
+  border: 2px solid transparent;
+  transition: 0.5s;
+
+  :hover {
+    border: 2px solid #343f53;
+  }
+`;
+
+const AdminLink = styled.a`
+  cursor: pointer;
+
+  text-decoration: none;
+  padding: 4px 0 4px 0;
+  font-family: 'Montserrat', sans-serif;
+  color: #343f53;
+  font-weight: 600;
+  font-size: 1rem;
+`;
+
 const RegistrationContainer = styled.div`
   display: flex;
   align-items: center;
@@ -72,6 +99,7 @@ const RegistrationContainer = styled.div`
 const FavouritesIcon = styled.img`
   padding: 16px 0 4px 0;
   width: 1.5rem;
+  cursor: pointer;
 `;
 const Divider = styled.span`
   padding: 16px 0 4px 0;
@@ -104,11 +132,15 @@ const Header: FunctionComponent = () => {
           </Link>
         </Pages>
         <RegistrationContainer>
-          <Link href="/user/myfavourites" passHref>
-            {/* should lead to user profile, if session doesn't exist, redirectTo login or register */}
-            <FavouritesIcon src="/icons/heart.svg" alt="Favourites icon" />
-          </Link>
-          <Divider>|</Divider>
+          {role !== 2 && (
+            <>
+              <Link href="/user/myfavourites" passHref>
+                {/* should lead to user profile, if session doesn't exist, redirectTo login or register */}
+                <FavouritesIcon src="/icons/heart.svg" alt="Favourites icon" />
+              </Link>
+              <Divider>|</Divider>
+            </>
+          )}
           {!username ? (
             <>
               <Link href="/register" passHref>
@@ -123,9 +155,11 @@ const Header: FunctionComponent = () => {
             <>
               {role === 2 && (
                 <>
-                  <Link href="/user/myshelter" passHref>
-                    <PageLink>admin</PageLink>
-                  </Link>
+                  <AdminContainer>
+                    <Link href="/user/myshelter" passHref>
+                      <AdminLink>my shelter</AdminLink>
+                    </Link>
+                  </AdminContainer>
                   <Divider>|</Divider>
                 </>
               )}
@@ -134,9 +168,6 @@ const Header: FunctionComponent = () => {
               </Link>
               <Divider>|</Divider>
               <PageLink href="/logout">Log out </PageLink>
-              {/* <Link href="/logout" passHref>
-                <PageLink>Log out</PageLink>
-              </Link> */}
             </>
           )}
         </RegistrationContainer>
